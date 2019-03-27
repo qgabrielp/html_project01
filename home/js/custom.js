6232a16bd6afd00388fd7a0c9d1afa73ec2b2f3c
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-03-27 17:19:40 +0900

    0327_06

--- home/js/custom.js
+++ home/js/custom.js
@@ -1,6 +1,19 @@
-﻿$(document).ready(function(){
-    
-});
+﻿//$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) { 
+            
+            $('header').css({'position': 'fixed','left': '50%','width': '50%','height': '5%','top': '0px'});
+            
+        } else {
+            $('header').css({'position': 'fixed','left': '50%','width': '30%','height': '10%','top': '10%'});
+        }
+//        if ($(this).scrollTop() < 50) { 
+//            
+//            $("#top_menubar_2").css({'position': 'inherit','margin-left': '-700px','width': '1400px','height': '100px'});
+//            
+//        } 
+    });
+//});
 
 
 
