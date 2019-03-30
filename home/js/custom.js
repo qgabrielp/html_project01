8fd9c7252c63d9ac559968677c2ba986dd4545c1
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-03-30 22:19:26 +0900

    0330_06

--- home/js/custom.js
+++ home/js/custom.js
@@ -5,12 +5,12 @@
         var page4top = $('.page4').offset().top;
         
         if ($(this).scrollTop() > page2top-50) {
-            $('header').stop().animate({'position': 'fixed','left': '0%','width': '100%','height': '5%','top': '0px','background-color':'rgba(255,255,255,0.6)'});
+            $('header').stop().animate({'position': 'fixed','left': '0%','width': '100%','height': '5%','top': '0px','background-color':'rgba(255,255,255,0.6)'}, 600);
             $('#menu>li').css({'float':'left'});
             $('#top_red').addClass('dnone');
             $('#logo').addClass('logo_sm');
         } else {
-            $('header').stop().animate({'position': 'fixed','left': '0%','width': '20%','height': '30%','top': '0%','background-color':'rgba(255,255,255,0.0)'});
+            $('header').stop().animate({'position': 'fixed','left': '0%','width': '20%','height': '30%','top': '0%','background-color':'rgba(255,255,255,0.0)'}, 600);
             $('#menu>li').css({'float':'none'});
             $('#top_red').removeClass('dnone');
             $('#logo').removeClass('logo_sm');
@@ -21,10 +21,10 @@
         }else{
             $('#home span').removeClass('on');
         }
-        if($(this).scrollTop() > page2top-50 && $(this).scrollTop() < page3top){
+        if($(this).scrollTop() > page2top-20 && $(this).scrollTop() < page3top){
             $('#skill span').addClass('on');
             $(".ch_tx").addClass('ct_tx_d');
-            $("#ch7").stop().animate({"width":"125px", "height":"125px"}, 1000);
+            $("#ch7").stop().animate({"width":"125px", "height":"125px"}, 600);
         }else{
             $('#skill span').removeClass('on');
             $(".ch_tx").removeClass('ct_tx_d');
@@ -56,7 +56,7 @@ $(document).ready(function(){
             e.preventDefault();
             $(".ch").gaugeMeter();
             $(".ch_tx").addClass('ct_tx_d');
-            $("#ch7").stop().animate({"width":"125px", "height":"125px"}, 1000);
+            $("#ch7").stop().animate({"width":"125px", "height":"125px"}, 600);
            }else{
                $(".ch").gaugeMeter();
                $(".ch_tx").removeClass('ct_tx_d');
