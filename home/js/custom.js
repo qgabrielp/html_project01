c9a3d5bf487660d151ea061bc0d054b3485943be
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-03-31 22:19:36 +0900

    0331_07

--- home/js/custom.js
+++ home/js/custom.js
@@ -6,11 +6,13 @@
         
         if ($(this).scrollTop() > page2top-50) {
             $('header').stop().animate({'position': 'fixed','left': '0%','width': '100%','height': '5%','top': '0px','background-color':'rgba(255,255,255,0.6)'}, 600);
+            $('#menu').css({'left':'10%'});
             $('#menu>li').css({'float':'left'});
             $('#top_red').addClass('dnone');
             $('#logo').addClass('logo_sm');
         } else {
             $('header').stop().animate({'position': 'fixed','left': '0%','width': '20%','height': '30%','top': '0%','background-color':'rgba(255,255,255,0.0)'}, 600);
+            $('#menu').css({'left':'0%'});
             $('#menu>li').css({'float':'none'});
             $('#top_red').removeClass('dnone');
             $('#logo').removeClass('logo_sm');
