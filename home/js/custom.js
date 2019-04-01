87b0214e2d13d5ad3dda778c451f06fa0e4916ea
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-04-01 17:19:11 +0900

    0401_05

--- home/js/custom.js
+++ home/js/custom.js
@@ -5,13 +5,17 @@
         var page4top = $('.page4').offset().top;
         
         if ($(this).scrollTop() > page2top-50) {
-            $('header').stop().animate({'position': 'fixed','left': '0%','width': '100%','height': '5%','top': '0px','background-color':'rgba(255,255,255,0.6)'}, 600);
+//            $('header').stop().animate({'position': 'fixed','left': '0%','width': '100%','height': '5%','top': '0px','background-color':'rgba(255,255,255,0.6)'}, 600);
+            $('header').removeClass('header_sm');
+            $('header').addClass('header_la');
             $('#menu').css({'left':'10%'});
             $('#menu>li').css({'float':'left'});
             $('#top_red').addClass('dnone');
             $('#logo').addClass('logo_sm');
         } else {
-            $('header').stop().animate({'position': 'fixed','left': '0%','width': '20%','height': '30%','top': '0%','background-color':'rgba(255,255,255,0.0)'}, 600);
+//            $('header').stop().animate({'position': 'fixed','left': '0%','width': '20%','height': '30%','top': '0%','background-color':'rgba(255,255,255,0.0)'}, 600);
+            $('header').addClass('header_sm');
+            $('header').removeClass('header_la');
             $('#menu').css({'left':'0%'});
             $('#menu>li').css({'float':'none'});
             $('#top_red').removeClass('dnone');
@@ -43,6 +47,11 @@
         }else{
             $('#contact span').removeClass('on');
         }
+        
+        $("#menu_btn").click(function(){
+            $("#menu2").show();
+        });
+        
     });
 
 $(document).ready(function(){
