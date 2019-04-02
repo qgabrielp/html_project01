ac36d6de4e7ed3184294f34be940fea9386e36ef
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-04-02 21:32:58 +0900

    0402_06

--- 0402 portfolio_03/js/custom.js
+++ 0402 portfolio_03/js/custom.js
@@ -1,10 +1,13 @@
 ﻿$(document).ready(function(){
     $("#menu_u>li>ul").hide();
+    $("#menu_bg").hide();
     $("#menu_u>li").hover(function(){
         var i = $(this).index();
-        $("#menu_u>li>ul").stop().slideDown();
+        $("#menu_u>li>ul").stop().slideDown(300);
+        $("#menu_bg").stop().slideDown(300);
     }).mouseleave(function(){
-        $("#menu_u>li>ul").stop().slideUp();
+        $("#menu_u>li>ul").stop().slideUp(300);
+        $("#menu_bg").stop().slideUp(300);
     });
 });
 
