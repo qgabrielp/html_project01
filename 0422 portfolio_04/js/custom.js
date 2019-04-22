c6f659042c8e88e17aa045fa5740ae9b02b62f2b
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-04-22 18:25:56 +0900

    0422_04

--- 0422 portfolio_04/js/custom.js
+++ 0422 portfolio_04/js/custom.js
@@ -1,1 +1,15 @@
-﻿
+﻿$(document).ready(function(){
+    new WOW().init();
+    
+    $('.menu_text').click(function(){
+        $('.menu_text').stop().fadeOut(1);
+        $('.close_text').stop().fadeIn(100);
+        $('.menu').stop().fadeIn();
+        new WOW().init();
+    });
+    $('.close_text').click(function(){
+        $('.close_text').stop().fadeOut(1);
+        $('.menu_text').stop().fadeIn(100);
+        $('.menu').stop().fadeOut();
+    });
+});
