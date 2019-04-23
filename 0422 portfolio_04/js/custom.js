33d50f8088424888411241e342c5127d7422d15e
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-04-23 12:35:56 +0900

    0423_02

--- 0422 portfolio_04/js/custom.js
+++ 0422 portfolio_04/js/custom.js
@@ -5,10 +5,25 @@
         $('.menu_text').stop().fadeOut(1);
         $('.close_text').stop().fadeIn(100);
         $('.menu').stop().fadeIn();
+        $('.kren').stop().fadeIn(1000);
+        $('.rei').stop().fadeIn(1000);
+        $('.gr_sow').addClass('cross_gr');
+        $('.red_sow').addClass('cross_red');
     });
     $('.close_text').click(function(){
         $('.close_text').stop().fadeOut(1);
         $('.menu_text').stop().fadeIn(100);
         $('.menu').stop().fadeOut();
+        $('.kren').stop().fadeOut();
+        $('.rei').stop().fadeOut();
+        $('.gr_sow').removeClass('cross_gr');
+        $('.red_sow').removeClass('cross_red');
+    });
+    
+    var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
     });
 });
