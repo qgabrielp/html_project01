8fcd38b2ec4f947ef95132a349f70b78839dd7a3
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-05-19 01:04:59 +0900

    0519_01

--- 0518 portfolio_05/js/custom.js
+++ 0518 portfolio_05/js/custom.js
@@ -1,7 +1,17 @@
 ﻿$(document).ready(function(){
-
-	// 실제 스크립트 코드를 입력할 공간입니다.
-	
+    $('.bn_2').hide();
+    $('.bn_btn_1').click(function(){
+        $('.bn_btn_1').addClass('on');
+        $('.bn_btn_2').removeClass('on');
+        $('.bn_2').hide();
+        $('.bn_1').fadeIn(600);
+    });
+    $('.bn_btn_2').click(function(){
+        $('.bn_btn_2').addClass('on');
+        $('.bn_btn_1').removeClass('on');
+        $('.bn_1').hide();
+        $('.bn_2').fadeIn(600);
+    });
 });
 
 
