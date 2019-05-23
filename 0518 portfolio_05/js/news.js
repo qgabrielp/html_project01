d90ebc0ef6f3ce0f8a8deec93aa0ae55a9d6fe28
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-05-23 22:27:29 +0900

    0523_04

---
+++ 0518 portfolio_05/js/news.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 50){
+                $('.topgu').stop().fadeIn();
+            }else{
+                $('.topgu').stop().fadeOut();
+            }
+    });
+    
+    $('.top').click(function(){
+        $('html, body').stop().animate({scrollTop : 0}, 400);
+        return false;
+    });
+    
+    $('#mmbtn').click(function(){
+        $('.main_menu').stop().animate({'left' : '0%',}, 600);
+        $('.bg').stop().fadeIn();
+        
+    });
+    $('.close').click(function(){
+        $('.main_menu').stop().animate({'left' : '100%'}, 600);
+        $('.bg').stop().fadeOut();
+    });
+    $('.main_menu>ul>li').click(function(){
+        $('.main_menu').stop().animate({'left' : '100%'}, 600);
+    });
+    
+});
