bb7d639778d022acc84e77ff6d69fcffa053e8d5
qgabrielp <47733556+qgabrielp@users.noreply.github.com>
2019-05-23 21:03:36 +0900

    0523_04

--- 0518 portfolio_05/js/qa.js
+++ 0518 portfolio_05/js/qa.js
@@ -12,4 +12,17 @@ $(document).ready(function(){
         return false;
     });
     
+    $('#mmbtn').click(function(){
+        $('.main_menu').stop().animate({'left' : '0%',}, 600);
+        $('.bg').stop().fadeIn();
+        
+    });
+    $('.close').click(function(){
+        $('.main_menu').stop().animate({'left' : '100%'}, 600);
+        $('.bg').stop().fadeOut();
+    });
+    $('.main_menu>ul>li').click(function(){
+        $('.main_menu').stop().animate({'left' : '100%'}, 600);
+    });
+    
 });
